9c3a71d26c4b40484b960df4ed7c9c496886b336
Igor Frecautan <frecautan.dev@gmail.com>
2023-02-23 21:46:13 +0200

    removed forward ref

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { ElementType, forwardRef, Ref } from "react";
+import React, { ElementType } from "react";
 import clsx from "clsx";
 
 import type { ButtonProps } from "./Button.types";
@@ -6,42 +6,33 @@ import styles from "./Button.module.scss";
 
 const defaultElement = "button" as const;
 
-export const Button = forwardRef(
-  <E extends ElementType = typeof defaultElement>(
-    {
-      children,
-      className,
-      as,
-      size = "normal",
-      variant = "red",
-      ...otherProps
-    }: ButtonProps<E>,
-    ref: Ref<E>
-  ): JSX.Element => {
-    const TagName = as || defaultElement;
-    return (
-      // @ts-ignore
-      <TagName
-        {...otherProps}
-        ref={ref}
-        className={clsx(
-          styles.btn,
-          {
-            [styles.normal]: size === "normal",
-            [styles.small]: size === "small",
-          },
-          {
-            [styles.red]: variant === "red",
-            [styles.green]: variant === "green",
-            [styles.white]: variant === "white",
-          },
-          className
-        )}
-      >
-        {children}
-      </TagName>
-    );
-  }
-);
-
-Button.displayName = "Button";
+export const Button = <E extends ElementType = typeof defaultElement>({
+  children,
+  className,
+  as,
+  size = "normal",
+  variant = "red",
+  ...otherProps
+}: ButtonProps<E>): JSX.Element => {
+  const TagName = as || defaultElement;
+  return (
+    <TagName
+      {...otherProps}
+      className={clsx(
+        styles.btn,
+        {
+          [styles.normal]: size === "normal",
+          [styles.small]: size === "small",
+        },
+        {
+          [styles.red]: variant === "red",
+          [styles.green]: variant === "green",
+          [styles.white]: variant === "white",
+        },
+        className
+      )}
+    >
+      {children}
+    </TagName>
+  );
+};
