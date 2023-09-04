32e0e3824fa575172fec02cda372d0978b5b3233
Igor Frecautan <frecautan.dev@gmail.com>
2023-09-05 01:48:25 +0300

    added amo test

--- pages/api/offer.ts
+++ pages/api/offer.ts
@@ -128,12 +128,54 @@ const handler: NextApiHandler = async (req, res) => {
           urlToFiles?.map((url) => ({
             type: 'document',
             media: url,
-            caption: `Fisiere atasate (${fields?.name})`,
           }))
         );
       }
 
-      res.status(200).send({ name: fields?.name, files: urlToFiles });
+      // Amo CRM
+      const { name, email, phone, ...restFields } = fields;
+      const data = JSON.stringify({
+        token: '7cc418e5-ad94-42cc-affd-13f5c0e99a98',
+        client_id: 30248032,
+        contact: {
+          name,
+          email,
+          phone,
+        },
+        fields: {
+          files: urlToFiles,
+          ...restFields,
+        },
+      });
+
+      const config = {
+        method: 'POST',
+        url: 'https://platon.progression.md/v4/app/forms.crm.add',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data,
+      };
+
+      const amo = await fetch(config.url, {
+        method: config.method,
+        headers: config.headers,
+        body: config.data,
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok AMO');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          return data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+      res.status(200).send({ name: fields?.name, files: urlToFiles, amo });
     } catch (error) {
       res.status(500).send(error);
     }
--- src/containers/OfferContainer/components/ThirdStep/ThirdStep.tsx
+++ src/containers/OfferContainer/components/ThirdStep/ThirdStep.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import clsx from 'clsx';
 import { Label } from 'UI';
@@ -6,18 +6,41 @@ import { useFormContext } from 'react-hook-form';
 
 export const ThirdStep: FC = (): JSX.Element => {
   const { setValue, watch } = useFormContext();
+  const [isError, setError] = useState<boolean>(false);
 
   const files = watch('files');
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop: (acceptedFiles: File[]) => {
-      setValue('files', acceptedFiles);
-    },
-    accept: {
-      'image/jpeg': ['.jpeg', '.png'],
-      'application/pdf': ['.pdf'],
-    },
-  });
+  const { getRootProps, getInputProps, isDragActive, fileRejections } =
+    useDropzone({
+      onDrop: (acceptedFiles: File[]) => {
+        const totalSize = acceptedFiles.reduce(
+          (acc: number, file: File) => acc + file.size,
+          0
+        );
+
+        if (totalSize > 30_000_000) {
+          setError(true);
+        } else {
+          setValue('files', acceptedFiles);
+        }
+      },
+      accept: {
+        'image/jpeg': ['.jpeg', '.jpg'],
+        'image/png': ['.png'],
+        'application/pdf': ['.pdf'],
+      },
+      maxSize: 5_000_000,
+      maxFiles: 20,
+    });
+
+  useEffect(() => {
+    if (fileRejections?.length > 0) {
+      setError(true);
+      setValue('files', []);
+    } else {
+      setError(false);
+    }
+  }, [fileRejections]);
 
   return (
     <>
@@ -37,17 +60,22 @@ export const ThirdStep: FC = (): JSX.Element => {
           <p>Drop the files here ...</p>
         ) : (
           <div>
-            <p className="font-medium">
-              Drag &apos;n&apos; drop some files here, or click to select files
+            <p className="font-medium text-sm mb-1">
+              Caută pe dispozitiv sau &apos;trage&apos; fișierele aici...
             </p>
-            <p className="text-sm text-gray-400">
-              (Only *.jpeg, *.png and *.pdf images will be accepted)
+            <p className="text-xs text-gray-400">
+              Se accepta doar formatele: *.JPG, *.PNG sau *.PDF. Limită: 20MB
+              per fișier / 30MB toate fișierele. Puteti atasa maxim 20 fișiere
             </p>
           </div>
         )}
       </button>
 
-      {files && (
+      <p className="text-cardinal text-sm font-semibold">
+        {isError ? 'Eroare' : ''}
+      </p>
+
+      {files?.length > 0 && (
         <ul className="list-disc pl-5">
           {files.map((file: File) => (
             <li className="text-sm text-atoll my-1" key={file.name}>
