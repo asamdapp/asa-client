36c90441ad79ac298a0bcd45e11601e90cdcebd4
Igor Frecautan <frecautan.dev@gmail.com>
2023-09-20 12:54:25 +0300

    added amo chat button codde

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -52,6 +52,20 @@ export default function Document() {
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WWGJ4X6');`,
           }}
         />
+
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(a,m,o,c,r,m){a[m]={id:"222001",hash:"431f3d1ac8c0fcb23f42d4965949fb080b490affc731e29498e43fa325b5adf2",locale:"ru",inline:false,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js?1695203391';d.head&&d.head.appendChild(s)}(window,0,'amoSocialButton',0,0,'amo_social_button'));`,
+          }}
+        />
+
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(a,m,o,c,r,m){a[m]={id:"222001",hash:"431f3d1ac8c0fcb23f42d4965949fb080b490affc731e29498e43fa325b5adf2",locale:"ru",inline:true,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js?1695203391';d.head&&d.head.appendChild(s)}(window,0,'amoSocialButton',0,0,'amo_social_button'));`,
+          }}
+        />
       </Head>
 
       <body>
