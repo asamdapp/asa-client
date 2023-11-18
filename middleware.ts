41d5a1ed3bfa3f898f97af62917a8c73a4262d92
Igor Frecautan <frecautan.dev@gmail.com>
2023-11-18 18:30:25 +0200

    removed middleware function

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,20 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
+// import { NextResponse } from 'next/server';
+//
+// export function middleware(request: NextRequest) {
+//   if (
+//     request.nextUrl.pathname.startsWith('/thanks') &&
+//     !request.cookies.get('thanks')
+//   ) {
+//     return NextResponse.redirect(
+//       new URL(request.nextUrl.locale === 'ru' ? '/ru' : '/', request.url)
+//     );
+//   }
+//   return NextResponse.next();
+// }
+//
+// export const config = {
+//   matcher: '/((?!api/).*)',
+// };
 
-export function middleware(request: NextRequest) {
-  if (
-    request.nextUrl.pathname.startsWith('/thanks') &&
-    !request.cookies.get('thanks')
-  ) {
-    return NextResponse.redirect(
-      new URL(request.nextUrl.locale === 'ru' ? '/ru' : '/', request.url)
-    );
-  }
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: '/((?!api/).*)',
-};
+export {};
