750f3e88e28c325bcb269dfac8f5e3dcb463b285
Igor Frecautan <93873911+frecautan@users.noreply.github.com>
2023-12-06 05:31:30 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,7 +31,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         page: (locale === 'ru' ? 'ru' : '') + asPath,
       },
     });
-  }, [asPath, locale]);
+  }, []);
 
   return (
     <>
